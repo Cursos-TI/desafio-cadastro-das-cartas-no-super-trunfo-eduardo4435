c2c887d903a63ed0e8c65c933bd2607b17858244
Eduardo Cruz <eduardo021cruz@gmail.com>
2025-03-30 15:31:51 +0000

    terceiro commit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,83 +8,33 @@ void limpar_entrada() {
 
 
 int main() {
-    char estado1, estado2 = 'a';
-    char codigoDaCarta1[10], codigoDaCarta2[10] = "vazio";
-    char nomeDaCidade1[10], nomeDaCidade2[10] = "vazio";
-    int populacao1, populacao2 = 0;
-    float area1, area2 = 0;
-    float pib1, pib2 = 0;
-    int pontosTuristicos1, pontosTuristicos2 = 0;
+    char estado;
+    char codigoDaCarta[10];
+    char nomeDaCidade[20];
+    int populacao;
+    float area;
+    float pib;
+    int pontosTuristicos;
 
+    printf("Digite os dados da primeira carta:\n");
+    printf("Estado (A a H): ");
+    scanf(" %c", &estado);
+    printf("Código da Carta (ex: A01): ");
+    scanf("%s", codigoDaCarta);
+    printf("Nome da Cidade: ");
+    getchar();
+    fgets(nomeDaCidade, sizeof(nomeDaCidade), stdin);
+    nomeDaCidade[strcspn(nomeDaCidade, "\n")] = '\0';
+    printf("População: ");
+    scanf("%d", &populacao);
+    printf("Área (em km²): ");
+    scanf("%f", &area);
+    printf("PIB (em bilhões de reais): ");
+    scanf("%f", &pib);
+    printf("Número de Pontos Turísticos: ");
+    scanf("%d", &pontosTuristicos);
 
-    //Leitura da primeira cidade: 
-
-    printf("ADICIONE AS INFORMACOES DA CARTA 1\n");
-
-    printf("Informe o estado: ");
-    limpar_entrada();
-    scanf(" %c", estado1);
-    printf("Informe o codigo: ");
-    limpar_entrada();
-    scanf(" %s", codigoDaCarta1);
-    printf("Informe o nome da cidade: ");
-    limpar_entrada();
-    fgets(nomeDaCidade1, 20, stdin);
-    printf("Informe a população da cidade: ");
-    limpar_entrada();
-    scanf(" %d", &populacao1);
-    printf("Informe a área em metros quadrados: ");
-    limpar_entrada();
-    scanf(" %f", &area1);
-    printf("Informe o PIB da cidade: ");
-    limpar_entrada();
-    scanf(" %f", &pib1);
-    printf("Informe a quantidade de pontos turisticos:");
-    limpar_entrada();
-    scanf(" %d", &pontosTuristicos1);
-
-    //leitura da segunda cidade:
-
-    printf("ADICIONE AS INFORMAÇÕES DA CARTA 2\n");
-    printf("Informe o estado: ");
-    limpar_entrada();
-    scanf("%c\n", &estado2);
-    printf("Informe o codigo: ");
-    limpar_entrada();
-    scanf("%s\n", codigoDaCarta2);
-    printf("Informe o nome da cidade: ");
-    limpar_entrada();
-    fgets(nomeDaCidade2, 20, stdin);
-    printf("Informe a população da cidade: ");
-    limpar_entrada();
-    scanf("%d\n", &populacao2);
-    printf("Informe a área em metros quadrados: ");
-    limpar_entrada();
-    scanf("%f\n", &area2);
-    printf("Informe o PIB da cidade: ");
-    limpar_entrada();
-    scanf("%f\n", &pib2);
-    printf("Informe a quantidade de pontos turisticos:");
-    limpar_entrada();
-    scanf("%d\n", &pontosTuristicos2);
-
-    printf("Carta 1:\n");
-    printf("Estado: %c\n", estado1);
-    printf("Codigo: %s\n", codigoDaCarta1);
-    printf("Nome da cidade: %s\n", nomeDaCidade1);
-    printf("População: %d\n", populacao1);
-    printf("Area: %f\n", area1);
-    printf("PIB: %f\n", pib1);
-    printf("Pontos turisticos: %d\n", pontosTuristicos1);
-
-    printf("Carta 2:\n");
-    printf("Estado: %c\n", estado2);
-    printf("Codigo: %s\n", codigoDaCarta2);
-    printf("Nome da cidade: %s\n", nomeDaCidade2);
-    printf("População: %d\n", populacao2);
-    printf("Area: %f\n", area2);
-    printf("PIB: %f\n", pib2);
-    printf("Pontos turisticos: %d\n", pontosTuristicos2);
+    
 
     return 0;
 }
